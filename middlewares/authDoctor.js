b5f9710b9b927cebd54820806ff45cb09c429a3a
Fraz <mallickfraz1234@gmail.com>
2024-10-25 21:29:46 +0530

    implemented authorization for doctor

---
+++ middlewares/authDoctor.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+
+// doctor authentication middleware
+
+const authDoctor = async (req, res, next) => {
+  try {
+    console.log(req.headers);
+    const { dtoken } = req.headers;
+    if (!dtoken) {
+      return res.json({
+        success: false,
+        message: "Not Authorized Login Again",
+      });
+    }
+    console.log("dtoken: ", dtoken);
+    const token_decode = jwt.verify(dtoken, process.env.JWT_SECRET);
+    req.body.docId = token_decode.id;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export default authDoctor;
